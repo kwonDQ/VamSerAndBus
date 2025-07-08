ae377722b4e795422ae716cf220098627801c3a0
kwonDQ <103370980+kwonDQ@users.noreply.github.com>
2025-07-08 17:53:41 +0900

    EnemySpawnSystem
    EnemySpawnSystem

---
+++ Assets/Scripts/EnemyPoolManager.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPoolManager : MonoBehaviour
+{
+    [System.Serializable] // EnemyPool을 클래스 유니티 에디터 인스펙터 창에 보이도록
+    public class EnemyPool // Enemy를 미리 생성해놓고 저장하는 클래스
+    {
+        public GameObject EnemyPrefab;
+        public int PoolSize = 10;
+        [HideInInspector] public Queue<GameObject> PoolQueue = new Queue<GameObject>();
+    }
+
+    public static EnemyPoolManager Instance; // 싱글톤으로 전역 접근 << 다른 스크립트에서 이 스크립트 접근 위해서
+
+    public EnemyPool[] EnemyPools; // 인스펙터창에서 구성요소 설정해야함 EnemyPrefab는 이 맵에서 쓸 적들 모두 넣고 PoolSize는 적당히 최적화되게
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this; // 이게 붙은 오브젝트 EnemyPoolManager가 대표 인스턴스가 됨
+        else Destroy(gameObject);
+
+        InitializePools();
+    }
+
+    void InitializePools() // 미리 적을 소환해서 준비시키는 함수
+    {
+        foreach (var pool in EnemyPools) // EnemyPools에 있는 모든 프리펩을 PoolSize개수 만큼 EnemyPrefab의 클론 오브젠트 생성
+        {
+            for (int i = 0; i < pool.PoolSize; i++)
+            {
+                GameObject obj = Instantiate(pool.EnemyPrefab);
+                obj.SetActive(false);
+                pool.PoolQueue.Enqueue(obj);
+            }
+        }
+    }
+
+    public GameObject GetEnemy(GameObject prefab) // 풀의 적을 활성화하는 함수
+    {
+        EnemyPool pool = FindPool(prefab);
+        if (pool == null) return null;
+
+        if (pool.PoolQueue.Count > 0)
+        {
+            GameObject enemy = pool.PoolQueue.Dequeue();
+            enemy.SetActive(true);
+            return enemy;
+        }
+        else
+        {
+            GameObject enemy = Instantiate(prefab); // 풀에 여유 없을 경우 생성 (옵션)
+            return enemy;
+        }
+    }
+
+    public void ReturnEnemy(GameObject prefab, GameObject enemy) // 풀의 적을 비활성화하는 함수
+    {
+        enemy.SetActive(false);
+        EnemyPool pool = FindPool(prefab);
+        if (pool != null)
+            pool.PoolQueue.Enqueue(enemy);
+        else
+            Destroy(enemy); // 풀을 못 찾으면 파괴
+    }
+
+    private EnemyPool FindPool(GameObject prefab) // 풀에 여유가 있는지, 들어갈 풀이 있는지 확인
+    {
+        foreach (var pool in EnemyPools) // EnemyPools에 있는 프리펩중에 준비되있는 풀 찾아서 있으면 반환 없으면 null
+        {
+            if (pool.EnemyPrefab.name == prefab.name)
+                return pool;
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/EnemySpawnZone.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnZone : MonoBehaviour
+{
+    public GameObject[] EnemyPrefabs;
+    public float SpawnInterval;
+    public float SpawnDistance;
+    public float SpawnAngleStart; // 0도가 x+ 방향, 시계방향으로 커짐
+    public float SpawnAngleEnd;
+    public float DespawnDistance;
+
+    private GameObject Player;
+    private Coroutine SpawnRoutine;
+    private List<EnemyInfo> ActiveEnemies = new List<EnemyInfo>();
+
+    class EnemyInfo
+    {
+        public GameObject EnemyObject;
+        public GameObject EnemyPrefab; // 어떤 프리팹으로 만들어졌는지 추적
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player = other.gameObject;
+            SpawnRoutine = StartCoroutine(SpawnEnemies());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StopCoroutine(SpawnRoutine);
+            // DespawnAll();
+            // Player = null;
+        }
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            if (EnemyPrefabs.Length > 0)
+            {
+                // 플레이어 기준 랜덤 방향에서 일정 거리 떨어진 위치
+                float angle = Random.Range(SpawnAngleStart * Mathf.PI / 180, SpawnAngleEnd * Mathf.PI / 180);
+                Vector2 offset = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * SpawnDistance;
+                Vector2 spawnPos = (Vector2)Player.transform.position + offset;
+
+                GameObject enemyPrefab = EnemyPrefabs[Random.Range(0, EnemyPrefabs.Length)];
+                GameObject enemy = EnemyPoolManager.Instance.GetEnemy(enemyPrefab);
+
+                enemy.transform.position = spawnPos;
+                enemy.transform.rotation = Quaternion.identity;
+
+                ActiveEnemies.Add(new EnemyInfo { EnemyObject = enemy, EnemyPrefab = enemyPrefab });
+            }
+            yield return new WaitForSeconds(SpawnInterval);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = ActiveEnemies.Count - 1; i >= 0; i--)
+        {
+            var enemyInfo = ActiveEnemies[i];
+            if (Vector3.Distance(Player.transform.position, enemyInfo.EnemyObject.transform.position) > DespawnDistance)
+            {
+                EnemyPoolManager.Instance.ReturnEnemy(enemyInfo.EnemyPrefab, enemyInfo.EnemyObject);
+                ActiveEnemies.RemoveAt(i);
+            }
+        }
+    }
+    /**
+    void DespawnAll()
+    {
+        foreach (var enemyInfo in ActiveEnemies)
+        {
+            EnemyPoolManager.Instance.ReturnEnemy(enemyInfo.EnemyPrefab, enemyInfo.EnemyObject);
+        }
+        ActiveEnemies.Clear();
+    }
+    */
+}
